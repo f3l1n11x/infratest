0e1ef09a55520dc1721404c8ade86ced86be220d
Luis Felipe Castilla Y <felipe.castilla@gmai.com>
2020-03-31 16:42:14 -0500

    matemticas

--- main.tf
+++ main.tf
@@ -8,3 +8,13 @@ pasando sengundo cambio
    \ \ / ,. \ / /
     ) '| || |' ( mrf
 OoO'- OoO''OoO -'OoO
+
+  (\(~)/)
+   )@_@(  #
+  ((q_p))'
+  /\|U|/\
+ /_o`='(_\
+((_)O oO O)
+ \O )o( O/
+  \(`-')/
+  /_] [_\ hjw
